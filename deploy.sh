cdf88891a23e6eeb4acc39d0fa7da45f440338cf
Meron Brouwer <meron.brouwer@gmail.com>
2017-07-20 12:29:14 +0200

    #1 Setting up deployment through gh-pages

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/env bash
+
+set -o errexit -o nounset
+
+if [ -f ~/.bash_tokens ]; then
+    source ~/.bash_tokens
+fi
+
+rev=$(git rev-parse --short HEAD)
+
+cd dist/
+
+git init
+git config user.name "Travis CI"
+git config user.email "noreply@travis.ci"
+
+git remote add upstream "https://$GH_TOKEN@github.com/meronbrouwer/oose-dea-eai-client.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+touch .
+touch .nojekyll
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
