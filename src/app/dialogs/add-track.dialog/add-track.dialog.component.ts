574dd2da37f478afab4365cdcc99747d1f78e51d
Meron Brouwer <meron.brouwer@gmail.com>
2017-08-31 11:38:29 +0200

    Introduced the Material table for displaying tracks

--- src/app/dialogs/add-track.dialog/add-track.dialog.component.ts
+++ src/app/dialogs/add-track.dialog/add-track.dialog.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {MdDialogRef} from '@angular/material';
 import {Track} from '../../models/track/track.interface';
 import {TrackImpl} from '../../models/track/track.model';
@@ -6,24 +6,26 @@ import {TrackService} from '../../services/track/track.service';
 import {Playlist} from '../../models/playlist/playlist.interface.model';
 import {Tracks} from '../../models/tracks/tracks.interface.model';
 import {TracksImpl} from '../../models/tracks/tracks.model';
+import {DataSource} from '@angular/cdk/collections';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 @Component({
   templateUrl: './add-track.dialog.component.html'
 })
-export class AddTrackDialogComponent implements OnInit {
+export class AddTrackDialogComponent {
 
   public track: Track;
   public tracks: Tracks;
   public playlist: Playlist;
 
+  displayedColumns = ['title', 'performer', 'duration', 'album', 'playcount', 'publicationDate', 'description'];
+  dataSource = undefined;
+
   constructor(private dialogRef: MdDialogRef<AddTrackDialogComponent>,
               private trackService: TrackService) {
     this.track = new TrackImpl();
     this.setEmptyTracklists();
-  }
-
-  ngOnInit(): void {
-
   }
 
   public onOk(): void {
@@ -43,10 +45,32 @@ export class AddTrackDialogComponent implements OnInit {
 
   private setTracks(tracks: Tracks): void {
     console.log('Settings tracks on dialog: ', tracks);
+    this.dataSource = new TracksDataSource(tracks);
     this.tracks = tracks;
   }
 
   private setEmptyTracklists(): void {
     this.tracks = new TracksImpl();
+  }
+}
+
+/**
+ * Data source to provide what data should be rendered in the table. The observable provided
+ * in connect should emit exactly the data that should be rendered by the table. If the data is
+ * altered, the observable should emit that new set of data on the stream. In our case here,
+ * we return a stream that contains only one set of data that doesn't change.
+ */
+export class TracksDataSource extends DataSource<any> {
+
+  constructor(private tracks: Tracks) {
+    super();
+  }
+
+  /** Connect function called by the table to retrieve one stream containing the data to render. */
+  connect(): Observable<Track[]> {
+    return Observable.of(this.tracks.tracks);
+  }
+
+  disconnect() {
   }
 }
