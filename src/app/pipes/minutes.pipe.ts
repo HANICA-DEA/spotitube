55bed349e76d562f5d791a35fb3bb7046a9f2423
Niels Germeraad <germeraadn@gmail.com>
2023-12-22 11:45:51 +0100

    Fixed Duration display not showing more than 24 hours

--- src/app/pipes/minutes.pipe.ts
+++ src/app/pipes/minutes.pipe.ts
@@ -6,11 +6,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class MinutesPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    const date = new Date(null);
-    date.setSeconds(value);
-    const minutes = date.toISOString().substr(11, 8);
+    const seconds = value % 60;
+    const minutes = Math.floor(value / 60) % 60;
+    const hours = Math.floor(minutes / 60);
 
-    return minutes;
+    const secondsString = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    const minutesString = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    const hoursString = hours < 10 ? `0${hours}` : `${hours}`;
+    return `${hoursString}:${minutesString}:${secondsString}`;
+
   }
 
 }
